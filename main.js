d5626e6e5028e569c89539bb4eb6da170d4b51f2
Vindhya <vindhya.raviraj@gmail.com>
2018-06-16 16:35:18 -0400

    added all functionality for quiz

--- main.js
+++ main.js
@@ -1,27 +1,61 @@
 $(document).ready(function() {
 	console.log('ready!');
 
-	// get a reference to the database service
-	const database = firebase.database();
-	const dbRoot = database.ref('/');
+	let userDrinkTemp;
+	let userFlavour;
+	let userMilk;
+	let userTopping;
+
+	// displays an embedded google maps based on the passed in shopLocation URL
+	const displayMap = shopLocation => {
+		if (shopLocation) { // if shopLocation has a value
+			const locationHtml = `<iframe src="${shopLocation}" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>`;
+			$('.shop-location').html(locationHtml);
+		} else { // if shopLocation is undefined, i.e., there is no drink result
+			$('.shop-location').empty();
+		}
+	};
+
+	// prints a drink based on the provided arguments/parameters, userTopping global variable, and userDrinkTemp global variable
+	const displayDrink = (drink, shopName, shopLocation) => {
+		// console.log(drink, shopName, shopLocation, userTopping, userDrinkTemp);
+		let drinkTemp = '';
+		let topping = '';
+
+		if (userDrinkTemp != 'slush') {
+			drinkTemp = userDrinkTemp + ' ';
+		}
+
+		if (userTopping != 'no-topping') {
+			topping = ` with ${userTopping}`;
+		}
+
+		displayMap(shopLocation);
+		$('.message').text(`You should order a ${drinkTemp}${drink}${topping} from ${shopName}.`);
+	};
 
 	const sorryMessage = () => {
+		displayMap();
 		$('.message').text(`Sorry, we don't have any drinks for you based on your preferences. Please try changing some of your answers around.`);
 	};
 
 	// checks whether the user chosen topping exists in the topping object passed in and returns a boolean
-	const checkTopping = (toppings, userTopping) => {
+	const checkTopping = (toppings) => {
 		let hasTopping = false;
 
-		for (topping in toppings) {
-			if (topping === userTopping) hasTopping = true;
+		if (userTopping === 'no-topping') {
+			hasTopping = true;
+		} else {
+			for (topping in toppings) {
+				if (topping === userTopping) hasTopping = true;
+			}
 		}
 
 		return hasTopping;
 	};
 
-	// checks whether the passed in drink object meshes with the user parameters for weather, flavour, and milk preference
-	const checkDrinkMatch = (drink, userDrinkTemp, userFlavour, userMilk) => {
+	// checks whether the passed in drink object meshes with the user's inputted preferences (global variables)
+	const checkDrinkMatch = (drink) => {
 		let drinkMatch = false;
 		
 		if (
@@ -39,7 +73,10 @@ $(document).ready(function() {
 		return drinkMatch;
 	};
 
-	const getResults = (userDrinkTemp, userFlavour, userMilk, userTopping) => {
+	const getResults = () => {
+		// get a reference to the database service
+		const database = firebase.database();
+		const dbRoot = database.ref('/');
 		const drinkResults = {};
 
 		dbRoot.once("value", snapshot => {
@@ -53,28 +90,82 @@ $(document).ready(function() {
 				drinkResults[shop].drinks = {};
 				drinkResults[shop].location = childSnapshot.child('location').val();
 				drinkResults[shop].name = childSnapshot.child('name').val();
-				drinkResults.topping = userTopping;
-				console.log(drinkResults);
+				// console.log(drinkResults);
 
-				if (checkTopping(toppings, userTopping)) {
+				if (checkTopping(toppings)) {
 					for (drink in drinks) {
 						if (checkDrinkMatch(drinks[drink], userDrinkTemp, userFlavour, userMilk)) {
-							console.log('checkDrinkMatch evaluated to true!');
 							drinkResults[shop]['drinks'][drink] = drinks[drink];
 						}
 					}
-				} 
+				}
+
+				// if the drinks child of the shop is still empty, remove the shop from the drinkResults object
+				// is it bad to do this while interating over the childSnapshot :|
+				// console.log('pre-delete', drinkResults);
+				if ($.isEmptyObject(drinkResults[shop].drinks)) {
+					delete drinkResults[shop];
+				}
+				// console.log('post-delete', drinkResults);
 
 			});
-		});
 
-		// if drink results is empty then call sorryMessage()
+			displayResults(drinkResults);
+		});
 
 		return drinkResults;
 	};
 
-	const displayResults = (userDrinkTemp, userFlavour, userMilk, userTopping) => {
-		getResults(userDrinkTemp, userFlavour, userMilk, userTopping);
+	// returns the number of drinks in the passed in object
+	// AND adds a key to drink
+	const countDrinks = drinkResults => {
+		let count = 0;
+
+		for (shop in drinkResults) {
+			for (drink in drinkResults[shop]['drinks']) {
+				count++;
+				drinkResults[shop]['drinks'][drink].key = count;
+			}
+		}
+
+		return count;
+	};
+
+	const randomNumber = max => {
+		return Math.floor(Math.random() * max) + 1;
+	};
+
+	// prints a drink based on the possible results
+	const displayResults = drinkResults => {
+
+		const drinkNum = countDrinks(drinkResults);
+
+		if (drinkNum > 1) {
+			// get a random number and display the drink with that key
+			const randIndex = randomNumber(drinkNum);
+			console.log('randomNumber index', randIndex);
+			for (shop in drinkResults) {
+				for (drink in drinkResults[shop]['drinks']) {
+					if (drinkResults[shop]['drinks'][drink].key === randIndex) {
+						console.log(drinkResults[shop]['drinks'][drink].key);	
+						console.log(drink);
+						displayDrink(drink, drinkResults[shop].name, drinkResults[shop].location);
+					}
+				}
+			}
+
+		} else if (drinkNum === 1) {
+			// display the one drink result
+			for (shop in drinkResults) {
+				for (drink in drinkResults[shop]['drinks']) {
+					displayDrink(drink, drinkResults[shop].name, drinkResults[shop].location);
+				}
+			}
+		} else {
+			// display the sorry message
+			sorryMessage();
+		}
+
 	};
 
 	// do this stuff when the form is submitted
@@ -82,14 +173,14 @@ $(document).ready(function() {
 		event.preventDefault();
 
 		// grab the values that the user chose and put them in variables
-		const userDrinkTemp = $('input[name="weather"]:checked').val();
-		const userFlavour = $('input[name="flavour"]:checked').val();
-		const userMilk = $('input[name="milk"]:checked').val();
-		const userTopping = $('input[name="topping"]:checked').val();
+		userDrinkTemp = $('#weather-select').val();
+		userFlavour = $('#flavour-select').val();
+		userMilk = $('#milk-select').val();
+		userTopping = $('#topping-select').val();
 
-		console.log(userDrinkTemp, userFlavour, userMilk, userTopping);
+		console.log('user inputs:', userDrinkTemp, userFlavour, userMilk, userTopping);
 
-		displayResults(userDrinkTemp, userFlavour, userMilk, userTopping);
+		getResults();
 	});
 
 });
